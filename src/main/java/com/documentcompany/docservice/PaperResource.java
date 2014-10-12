44df3e14a0504abd209358e870fb50edea79fe95
Pawel Koperek <pkoperek@gmail.com>
2014-10-12 15:52:28 +0100

    using memcached cache

--- src/main/java/com/documentcompany/docservice/PaperResource.java
+++ src/main/java/com/documentcompany/docservice/PaperResource.java
@@ -1,5 +1,7 @@
 package com.documentcompany.docservice;
 
+import net.spy.memcached.MemcachedClient;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,13 +12,26 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.TEXT_PLAIN)
 public class PaperResource {
 
+    private final MemcachedClient memcachedClient;
+
+    public PaperResource(MemcachedClient memcachedClient) {
+        this.memcachedClient = memcachedClient;
+    }
+
     @GET
     public String getPage(
             @QueryParam("page") String page,
             @QueryParam("size") String size,
             @QueryParam("zoomFactor") String zoomFactor
     ) {
-        return "Some page text";
+        Object cached = memcachedClient.get(page + size + zoomFactor);
+        if (cached != null) {
+            return cached.toString();
+        }
+
+        // instead of returning static string, there should be some code which
+        // actually reads the paper file
+        return "Some page text ";
     }
 
 }
--- src/test/java/com/documentcompany/docservice/PaperResourceTest.java
+++ src/test/java/com/documentcompany/docservice/PaperResourceTest.java
@@ -1,10 +1,34 @@
 package com.documentcompany.docservice;
 
+import net.spy.memcached.MemcachedClient;
 import org.junit.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class PaperResourceTest {
+
+    private final MemcachedClient memcachedClient = mock(MemcachedClient.class);
+
+    @Test
+    public void shouldReturnMemcacheCachedPage() throws Exception {
+
+        // Given
+        String cached = "cached page";
+
+        String page = "p1";
+        String size = "A4";
+        String zoomFactor = "1.4";
+
+        given(memcachedClient.get(page + size + zoomFactor)).willReturn(cached);
+
+        // When
+        String pageContent = getPage(page, size, zoomFactor);
+
+        // Then
+        assertThat(pageContent).isEqualTo(cached);
+    }
 
     @Test
     public void shouldReturnSensibleString() throws Exception {
@@ -12,10 +36,14 @@ public class PaperResourceTest {
         // Given
 
         // When
-        String page = new PaperResource().getPage("page", "size", "zoomFactor");
+        String page = getPage("page", "size", "zoomFactor");
 
         // Then
         assertThat(page).isEqualTo("Some page text");
+    }
+
+    private String getPage(String page, String size, String zoomFactor) {
+        return new PaperResource(memcachedClient).getPage(page, size, zoomFactor);
     }
 
 }
