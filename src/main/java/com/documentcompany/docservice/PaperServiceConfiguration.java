0a83141dfdda4e6f0fce30ae9fc2427235d457c1
Pawel Koperek <pkoperek@gmail.com>
2014-10-12 15:59:36 +0100

    configuration using memcached address

--- src/main/java/com/documentcompany/docservice/PaperServiceConfiguration.java
+++ src/main/java/com/documentcompany/docservice/PaperServiceConfiguration.java
@@ -1,6 +1,21 @@
 package com.documentcompany.docservice;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class PaperServiceConfiguration extends Configuration {
+
+    @NotEmpty
+    private String memcachedAddress;
+
+    @JsonProperty
+    public String getMemcachedAddress() {
+        return memcachedAddress;
+    }
+
+    @JsonProperty
+    public void setMemcachedAddress(String memcachedAddress) {
+        this.memcachedAddress = memcachedAddress;
+    }
 }
