41293d6169cd871420fbf427e88b993d2e6eb94e
Pawel Koperek <pkoperek@gmail.com>
2014-10-12 15:00:45 +0100

    run the service in main method

--- src/main/java/com/documentcompany/docservice/PaperServiceApplication.java
+++ src/main/java/com/documentcompany/docservice/PaperServiceApplication.java
@@ -6,6 +6,10 @@ import io.dropwizard.setup.Environment;
 
 public class PaperServiceApplication extends Application<PaperServiceConfiguration> {
 
+    public static void main(String[] args) throws Exception {
+        new PaperServiceApplication().run(args);
+    }
+
     @Override
     public void initialize(Bootstrap<PaperServiceConfiguration> bootstrap) {
         // does nothing
